5ff537103fcc63c8b1047ff2e6b5960184b1e368
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-15 23:26:13 +0900

    [PGS] 둘만의 암호
    https://school.programmers.co.kr/learn/courses/30/lessons/155652

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,17 +7,7 @@
 
 import Foundation
 
-var problem = Number155651()
+var problem = Number155652()
 
-print(
-  problem.solution([
-    ["15:00", "17:00"],
-    ["16:40", "18:20"],
-    ["14:20", "15:20"],
-    ["14:10", "19:20"],
-    ["18:20", "21:20"]
-  ])
-)
-
-print(problem.solution([["09:10", "10:10"], ["10:20", "12:20"]]))
-print(problem.solution([["10:20", "12:30"], ["10:20", "12:30"], ["10:20", "12:30"]]))
+print(problem.solution("aukks", "wbqd", 5))
+print(problem.solution("ybcde", "az", 1))
