c7cd9aa83c8d937d0817fec7d7a542e3b20f1b32
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-15 19:13:48 +0900

    [PGS] 호텔 대실
    https://school.programmers.co.kr/learn/courses/30/lessons/155651

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,4 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+var problem: Solution = Number155651()
+
+print(
+  problem.answer(input: [
+    ["15:00", "17:00"],
+    ["16:40", "18:20"],
+    ["14:20", "15:20"],
+    ["14:10", "19:20"],
+    ["18:20", "21:20"]
+  ])
+)
+
+print(problem.answer(input: [["09:10", "10:10"], ["10:20", "12:20"]]))
+print(problem.answer(input: [["10:20", "12:30"], ["10:20", "12:30"], ["10:20", "12:30"]]))
