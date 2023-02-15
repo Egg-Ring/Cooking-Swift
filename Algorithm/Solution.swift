d083c247413869512c13aee439c371eb6e2d63e1
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-15 19:08:40 +0900

    [CHORE] Add `mutating` keyword to `answer` method

--- Algorithm/Solution.swift
+++ Algorithm/Solution.swift
@@ -13,5 +13,5 @@ protocol Solution {
   /// This method is used to build the logic with the input of the algorithm problem and derive the output.
   /// - Parameter input: Input value of the algorithm problem
   /// - Returns: Result
-  func answer(input: Any) -> Any?
+  mutating func answer(input: Any) -> Any?
 }
