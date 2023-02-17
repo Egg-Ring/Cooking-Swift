a8d72fd1585e235337801f49c7b6689a166c40b1
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-17 18:48:40 +0900

    [MOVE] Deque code to Deque.swift

--- Algorithm/programmers/whitehyun/무인도 여행.swift
+++ Algorithm/programmers/whitehyun/무인도 여행.swift
@@ -12,65 +12,6 @@ struct Point: Equatable {
   let y: Int
 }
 
-struct Deque<Element: Equatable> {
-  private var frontQueue = [Element]()
-  private var backQueue = [Element]()
-  
-  var isEmpty: Bool {
-    frontQueue.isEmpty && backQueue.isEmpty
-  }
-  
-  var count: Int {
-    frontQueue.count + backQueue.count
-  }
-  
-  mutating func appendLeft(_ element: Element) {
-    frontQueue.append(element)
-  }
-  
-  mutating func append(_ element: Element) {
-    backQueue.append(element)
-  }
-  
-  mutating func popLeft() -> Element? {
-    if !frontQueue.isEmpty {
-      return frontQueue.popLast()
-    }
-    backQueue.reverse()
-    let value = backQueue.popLast()
-    backQueue.reverse()
-    return value
-  }
-  
-  mutating func pop() -> Element? {
-    if !backQueue.isEmpty {
-      return backQueue.popLast()
-    }
-    frontQueue.reverse()
-    let value = frontQueue.popLast()
-    frontQueue.reverse()
-    return value
-    
-  }
-  
-  func contains(_ element: Element) -> Bool {
-    return frontQueue.contains(element) || backQueue.contains(element)
-  }
-  
-  mutating func removeAll() {
-    frontQueue.removeAll()
-    backQueue.removeAll()
-  }
-}
-
-extension Deque: CustomStringConvertible {
-  var description: String {
-    "\(frontQueue), \(backQueue)"
-  }
-}
-
-
-
 struct Number154540 {
   func solution(_ maps: [String]) -> [Int] {
     if maps.filter({$0.count != $0.filter({$0 == "X"}).count}).isEmpty { return [-1] }
