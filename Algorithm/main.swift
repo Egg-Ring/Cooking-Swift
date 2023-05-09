04382ce5f09ad2e541f9f02bbf503222b2110e69
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-05-09 23:47:15 +0900

    [PGS] 두 원 사이의 정수 쌍
    https://school.programmers.co.kr/learn/courses/30/lessons/181187

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -7,14 +7,10 @@
 
 import Foundation
 
-protocol Solution {
-  associatedtype Input
-  associatedtype Output
-  
-  func solution(_ input: Input) -> Output
-}
 
+let problem = Number181187()
 
-let problem = Number181188()
+let r1 = 5
+let r2 = 6
 
-print(problem.solution([[4, 5], [4, 8], [10, 14], [11, 13], [5, 12], [3, 7], [1, 4]]))
+print(problem.solution(r1, r2))
