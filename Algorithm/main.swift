6fa1bcae75237baf3b04814f8c758a4d2ac3a6fe
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-05-11 12:59:59 +0900

    [PGS] 과제 진행하기
    https://school.programmers.co.kr/learn/courses/30/lessons/176962

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 
-let problem = Number178870()
+let problem = Number176962()
 
 
-print(problem.solution([1, 2, 3, 4, 5] , 7) == [2, 3])
-print(problem.solution([1, 1, 1, 2, 3, 4, 5],  5) == [6, 6])
-print(problem.solution([2, 2, 2, 2, 2], 6) == [0, 2])
+
+print(problem.solution([["korean", "11:40", "30"], ["english", "12:10", "20"], ["math", "12:30", "40"]]) == ["korean", "english", "math"])
+print(problem.solution([["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]) == ["science", "history", "computer", "music"])
+print(problem.solution([["aaa", "12:00", "20"], ["bbb", "12:10", "30"], ["ccc", "12:40", "10"]]) == ["bbb", "ccc", "aaa"])
+print(problem.solution([["A", "00:00", "100"], ["B", "23:59", "100"], ["C", "22:18", "100"], ["D", "22:17", "100"]]) == ["A", "C", "B", "D"])
